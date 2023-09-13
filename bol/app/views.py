078243ec39d49f4eb4fcce2cc1846239488a7699
masoud_mac <masoudmolayem@gmail.com>
2023-09-13 21:54:59 +0200

    changes on html file

--- bol/app/views.py
+++ bol/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import requests
-
+from django.template.loader import render_to_string
 # Create your views here.
 def home(request):
     """
@@ -8,4 +8,9 @@ def home(request):
     """
     url="https://legendary-toffee.herokuapp.com/search?queryId=31&userId=dummyuser"
     response = requests.get(url).json()
+
+    rendered_html = render_to_string("sample.html", response)
+    # Save the rendered HTML to a file
+    # with open("rendered_sample.html", "w") as file:
+    #     file.write(rendered_html)
     return render(request, "sample.html", response)
